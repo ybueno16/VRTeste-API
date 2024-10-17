248d4bb11246af0738b5cb219385f055cb25ab71
ybueno16 <yuribueno27@gmail.com>
2024-10-16 21:28:51 -0300

    feat: adicionar teste de unitario validando erro

--- src/test/java/com/br/testeVr/matricula/controller/MatriculaControllerTest.java
+++ src/test/java/com/br/testeVr/matricula/controller/MatriculaControllerTest.java
@@ -12,8 +12,11 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.sql.SQLException;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MatriculaControllerTest {
@@ -46,6 +49,14 @@ public class MatriculaControllerTest {
     }
 
     @Test
+    public void testGetMatriculassComErro() throws Exception {
+
+        when(matriculaService.getMatriculas()).thenThrow(SQLException.class);
+        ResponseEntity<?> response = matriculaController.getMatriculas();
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+
+    @Test
     public void testCadastrarMatricula() throws Exception {
         doNothing().when(matriculaService).cadastrarMatricula(matricula);
         ResponseEntity<?> response = matriculaController.cadastrarMatricula(matricula);
@@ -53,9 +64,25 @@ public class MatriculaControllerTest {
     }
 
     @Test
+    public void testCadastrarMatriculassComErro() throws Exception {
+        doThrow(SQLException.class).when(matriculaService).cadastrarMatricula(matricula);
+        ResponseEntity<?> response = matriculaController.cadastrarMatricula(matricula);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+
+
+
+    @Test
     public void testRemoverMatricula() throws Exception {
         doNothing().when(matriculaService).removerMatricula(matricula.getId());
         ResponseEntity<?> response = matriculaController.removerMatricula(matricula.getId());
         assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void testRemoverMatriculassComErro() throws Exception {
+        doThrow(SQLException.class).when(matriculaService).removerMatricula(1L);
+        ResponseEntity<?> response = matriculaController.removerMatricula(1L);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
     }
 }
